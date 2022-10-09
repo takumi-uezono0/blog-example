295e5360cab9ac595669551f8f031cb6843ece4e
takumi-uezono0 <uezono1998@gmail.com>
2022-10-10 04:52:47 +0900

    fix: title

--- components/intro.tsx
+++ components/intro.tsx
@@ -3,7 +3,7 @@ import { CMS_NAME } from "../lib/constants";
 const Intro = () => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      hello world!
+      takumi uezono!
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
         A statically generated blog example using{" "}
         <a
